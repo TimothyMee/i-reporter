d379d8fd62afbe4dd175a2176eebdfb3b8790fbb
TimothyMee <timothy33.tf@gmail.com>
2018-11-29 12:18:52 +0100

    made changes to the routes file to show the available routes on failure

--- routes/routes.js
+++ routes/routes.js
@@ -8,24 +8,24 @@ router.use('/api/v1', require('./v1/incident.routes'));
 router.use('/*', express.Router()
   .all('/*', (req, res) => {
     res.status(403)
-      .send('use an appropriate route with the "../api/v1/.." before the routes \n'
-            + 'e.g "https://timothy-i-reporter.herokuapp.com/api/v1/red-flags"\n\n'
-            + 'Available API Routes are listed below: \n\n'
-            + 'Red Flags: \n'
-            + '  -GET https://timothy-i-reporter.herokuapp.com/api/v1/red-flags \n'
-            + '  -GET https://timothy-i-reporter.herokuapp.com/api/v1/red-flags/{id} \n'
-            + '  -POST https://timothy-i-reporter.herokuapp.com/api/v1/red-flags \n'
-            + '  -PATCH https://timothy-i-reporter.herokuapp.com/api/v1/red-flags/{id}/location \n'
-            + '  -PATCH https://timothy-i-reporter.herokuapp.com/api/v1/red-flags/{id}/comment \n'
-            + '  -DELETE https://timothy-i-reporter.herokuapp.com/api/v1/red-flags/{id} \n\n'
+      .send('use an appropriate route with the "../api/v1/.." before the routes \n <br>'
+            + 'e.g "https://timothy-i-reporter.herokuapp.com/api/v1/red-flags"\n\n <br><br>'
+            + 'Available API Routes are listed below: \n\n <br><br>'
+            + 'Red Flags: \n <br>'
+            + '  -GET https://timothy-i-reporter.herokuapp.com/api/v1/red-flags \n <br>'
+            + '  -GET https://timothy-i-reporter.herokuapp.com/api/v1/red-flags/{id} \n <br>'
+            + '  -POST https://timothy-i-reporter.herokuapp.com/api/v1/red-flags \n <br>'
+            + '  -PATCH https://timothy-i-reporter.herokuapp.com/api/v1/red-flags/{id}/location \n <br>'
+            + '  -PATCH https://timothy-i-reporter.herokuapp.com/api/v1/red-flags/{id}/comment \n <br>'
+            + '  -DELETE https://timothy-i-reporter.herokuapp.com/api/v1/red-flags/{id} \n\n <br><br>'
 
-            + 'Interventions: \n'
-            + '  -GET https://timothy-i-reporter.herokuapp.com/api/v1/interventions \n'
-            + '  -GET https://timothy-i-reporter.herokuapp.com/api/v1/interventions/{id} \n'
-            + '  -POST https://timothy-i-reporter.herokuapp.com/api/v1/interventions\n'
-            + '  -PATCH https://timothy-i-reporter.herokuapp.com/api/v1/interventions/{id}/location \n'
-            + '  -PATCH https://timothy-i-reporter.herokuapp.com/api/v1/interventions/{id}/comment \n'
-            + '  -DELETE https://timothy-i-reporter.herokuapp.com/api/v1/interventions/{id} \n');
+            + 'Interventions: \n <br>'
+            + '  -GET https://timothy-i-reporter.herokuapp.com/api/v1/interventions \n <br>'
+            + '  -GET https://timothy-i-reporter.herokuapp.com/api/v1/interventions/{id} \n <br>'
+            + '  -POST https://timothy-i-reporter.herokuapp.com/api/v1/interventions\n <br>'
+            + '  -PATCH https://timothy-i-reporter.herokuapp.com/api/v1/interventions/{id}/location \n <br>'
+            + '  -PATCH https://timothy-i-reporter.herokuapp.com/api/v1/interventions/{id}/comment \n <br>'
+            + '  -DELETE https://timothy-i-reporter.herokuapp.com/api/v1/interventions/{id} \n <br>');
   }));
 
 module.exports = router;
